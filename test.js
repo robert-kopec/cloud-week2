5cb3cf5446fb26d7a5ba5eba44705a2bd44cacc3
Robert Kopec <robert.kopec@ivanti.com>
2021-02-07 13:03:22 +0000

    message update

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-res.send('Hello Virtual Machine!')
+    res.send('Hello Nodes!')
 })
 
 app.listen(port, () => {
-console.log(`Express Application listening at port 3000`)
+    console.log(`Express Application listening at port 3000`)
 })
